776b0d76f4df82e9816867511e0c42c2ed88180f
glsunnykumar <45039183+glsunnykumar@users.noreply.github.com>
2025-02-15 10:44:45 +0530

    Adding the Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { CoursesComponent } from './pages/courses/courses.component';
+import { AchievementsComponent } from './pages/achievements/achievements.component';
+import { AdmissionComponent } from './pages/admission/admission.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+  { path: 'courses', component: CoursesComponent },
+  { path: 'achievements', component: AchievementsComponent },
+  { path: 'admission', component: AdmissionComponent },
+  { path: 'contact', component: ContactComponent },
+
+];
