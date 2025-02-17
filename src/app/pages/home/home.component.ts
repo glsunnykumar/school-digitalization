bdbdc30b185440df35cdfccc336e8a2ab554ce54
glsunnykumar <45039183+glsunnykumar@users.noreply.github.com>
2025-02-17 11:48:40 +0530

    before adding Carsouel

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,36 @@
 import { Component } from '@angular/core';
 import { MenubarComponent } from "../menubar/menubar.component";
 import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-home',
   imports: [MenubarComponent,
-    MatIconModule
+    MatIconModule,
+    MatButtonModule,
+    CommonModule
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  courses = [
+    {
+      name: 'Computer Science',
+      description: 'Learn programming, AI, and web development.',
+      image: '/assets/cs-course.jpg'
+    },
+    {
+      name: 'All Stream (Non Med)',
+      description: 'Arts Commerce and Science.',
+      image: '/assets/business-course.jpg'
+    },
+    {
+      name: 'Graphic Design',
+      description: 'Explore creativity with digital design tools.',
+      image: '/assets/design-course.jpg'
+    }
+  ];
 
 }
