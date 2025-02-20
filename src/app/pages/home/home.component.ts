e93174c7da852d1c5dbb8006b16c55f2c0b12273
glsunnykumar <45039183+glsunnykumar@users.noreply.github.com>
2025-02-20 10:12:12 +0530

    Before Adding Footer to The Application

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,20 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
 import { MenubarComponent } from "../menubar/menubar.component";
 import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { register } from 'swiper/element-bundle';
 
 @Component({
   selector: 'app-home',
   imports: [MenubarComponent,
     MatIconModule,
     MatButtonModule,
+    MatCardModule,
     CommonModule
   ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
+  schemas :[CUSTOM_ELEMENTS_SCHEMA]
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
   courses = [
     {
       name: 'Computer Science',
@@ -37,5 +41,19 @@ export class HomeComponent {
        image: '/assets/AI.png'
     }
   ];
+
+   // Top 5 team members
+   topMembers = [
+    { name: 'Nisha Kumari', role: 'Member', photo: 'assets/image.webp',designation :'Lect English' },
+    { name: 'Rajesh Kumar', role: 'Member', photo: 'assets/image.webp' ,designation :'Lect History' },
+    { name: 'Abodh Kumar', role: 'Member', photo: 'assets/image.webp',designation :'Lect Chemistry' },
+    { name: 'Ranju Sharma', role: 'Member', photo: 'assets/image.webp',designation :'Lect Pol Science' },
+    
+  
+  ];
+
+  ngOnInit(): void {
+    register();
+  }
 
 }
